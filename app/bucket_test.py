0f81e74bf31d53330c7de282a4dd40072e5d7e79
KevKipkemoi <kevkipkemoi@outlook.com>
2017-07-20 09:54:47 +0300

    Trial test

--- app/bucket_test.py
+++ app/bucket_test.py
@@ -3,6 +3,38 @@ from forms import Bucket
 
 
 class BucketTestCase(unittest.TestCase):
+    
+    #ClassIsSetup = False
+    username = 'kev'
+    password = 'czar'
+    goal = 'make a difference'
+    
+    @classmethod
+    def setUpClass(self):
+        unittest.TestCase.setUp(self)
+        self.Bucket = Bucket(username, password, goal)
+    
+    """    
+    def setUpClass(self):
+        unittest.TestCase.setUp(self)
+        Bucket(username, password, goal)
+        username = 'kev'
+        password = 'czar'
+        goal = 'make a difference'
+    """
+        
+
+    def test_add_user(self):
+        self.assertTrue(Bucket.add_user, 1)
+        
+    def test_invalid_user(self):
+        self.assertTrue(Bucket.invalid_user, 1)
+        
+    def test_login(self):
+        self.assertTrue(Bucket.login(username))
 
     def test_display_goal(self):
-        self.assertEqual()
+        self.assertEqual(self.user[username][1], goal)
+                
+if __name__ == '__main__':
+    unittest.main(exit=False)
