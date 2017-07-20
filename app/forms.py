2af5e86518e68e44c93cda017cda411def7f541e
Kevin Kipkemoi <Kevkipkemoi@outlook.com>
2017-07-20 11:31:25 +0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -31,8 +31,50 @@ class EditForm(Form):
 
 class Bucket(dict, LoginForm, GoalsForm):
 
-    def display_goal(self, username, password, body):
-        if username in users.keys() and password == user[username][0]:
+    def __init__(self, username, password, goal):
+        self.username = username
+        self.password = password
+        self.goal = goal
+    
+    remember = False
+    added_user = False
+    
+    def login(self, username):
+        if username in user.keys():
+            if password == users[username][0]:
+                print('login successful')
+                remember = True
+                return remember
+        else:
+            print('invalid username or password')
+            
+    def add_user(self, username, password):
+        if username not in users.keys():
+            user[username] = [password]
+            return 1
+
+    def invalid_user(self, username, password):
+        reset = 0
+        if not login(username):
+            self.users.update({username : [password]})
+            added_user = True
+            if added_user:
+                reset = 1
+                added_user = False
+            return reset
+                
+        
+    def display_goal(self, username):
+        if remember:
             return self.user[username][1]
         else:
-            self.user.update({username : [password, body]})
+            print('please login/signup')
+            
+    def add_goal(self, username, goal):      
+        if remember:
+            self.user[username][1].insert(goal)
+        else:
+            print('please login/signup')
+            
+    def logout(self, username):
+        remember = False
