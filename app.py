b003df995e4dab9ec7abc6d9f7f3f32f7a3dbc63
KevKipkemoi <kevkipkemoi@outlook.com>
2017-07-26 08:37:30 +0300

    updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
 
+@app.route('/')
+@app.route('/home')
 def main():
 	return render_template('index.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+	return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
