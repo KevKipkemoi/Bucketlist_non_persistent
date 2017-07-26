0e5a929ecb87acd224a1fc23d43a996f5a03589c
KevKipkemoi <kevkipkemoi@outlook.com>
2017-07-26 12:06:19 +0300

    added username field, pushed form data to views.py

--- app/views.py
+++ app/views.py
@@ -3,7 +3,24 @@ from flask import Flask, render_template
  
 app = Flask(__name__)
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template("index.html")
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	if request.method == 'GET':
+
+		return render_template("signup.html")
+
+	if request.method == 'POST':
+		name = reuqest.form.get('inputName')
