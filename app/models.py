5171b0fea564ba3a631b408e0b298ee3e700419b
KevKipkemoi <kevkipkemoi@outlook.com>
2017-07-26 13:31:12 +0300

    initial commit of models.py

---
+++ app/models.py
@@ -0,0 +1,20 @@
+class UserData(object):
+
+	def __init__(self, name, username, email, password):
+		self.name = name
+		self.username = username
+		self.email = email
+		self.password = password
+		self.userdata = []
+
+	def new_user_data(self, name, username, email, password):
+		self.userdata.append({
+			    'Name': name,
+			    'Password': password,
+			    'email': email,
+			    'User Name': username
+			})
+
+	def edit_user_data(self, name=None, username=None, email=None, password=None):
+		if name:
+			
